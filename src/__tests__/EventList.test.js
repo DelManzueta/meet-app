e8704362809eb1333230f78e17a10a0676afa4d2
DelManzueta <dmanzueta@gmail.com>
2021-05-20 13:49:39 -0400

    render list of suggestions correctly

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import Event from '../Event'
+import EventList from '../EventList'
+
+describe('<EventList /> component', () => {
+  test('render correct number of events', () => {
+    const EventListWrapper = shallow(
+      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+    )
+    expect(EventListWrapper.find(Event)).toHaveLength(4)
+  })
+})
